35882a7f9c31028695195c6f1dd6b85f902d1eae
ArfinoBahni <arfino.pratikno@binus.ac.id>
2024-12-17 20:01:38 +0700

    sql file fix

--- calouseif.sql
+++ calouseif.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2024 at 02:52 PM
+-- Generation Time: Dec 17, 2024 at 01:57 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -51,10 +51,73 @@ INSERT INTO `items` (`item_id`, `seller_id`, `item_name`, `item_category`, `item
 (5, 3, 'Short pants', 'Bottom', 'L', 30000, 'declined', 'none', 0, NULL, NULL),
 (7, 3, 'Long pants', 'Bottom', 'M', 30000, 'declined', 'accepted', 28000, 2, NULL),
 (8, 3, 'Jacket', 'top', 'M', 53000, 'declined', 'none', 0, NULL, NULL),
-(9, 3, 'Sweater', 'top', 'S', 40000, 'approved', 'pending', 32000, 4, NULL),
+(9, 3, 'Sweater', 'top', 'S', 40000, 'declined', 'accepted', 32000, 4, NULL),
 (10, 3, 'air jordang', 'shoes', '39', 80000, 'declined', 'none', 0, NULL, 'Deny request reason: Item in bad condition'),
 (11, 3, 'erforswan', 'shoes', '40', 40000, 'declined', 'accepted', 35000, 0, NULL),
-(12, 3, 'adadis', 'shoes', '40', 50000, 'declined', 'accepted', 45000, 0, 'Deny offer reason: too cheap');
+(12, 3, 'adadis', 'shoes', '40', 50000, 'declined', 'accepted', 45000, 0, 'Deny offer reason: too cheap'),
+(13, 3, 'Swallow', 'sandals', '38', 10000, 'approved', 'pending', 6000, 2, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) NOT NULL,
+  `buyer_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `transaction_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transaction_id`, `buyer_id`, `item_id`, `transaction_date`) VALUES
+(1, 2, 2, '2024-12-07 09:44:29'),
+(3, 2, 5, '2024-12-07 10:09:59'),
+(4, 2, 7, '2024-12-08 09:16:52'),
+(5, 2, 8, '2024-12-10 09:55:54'),
+(8, 4, 9, '2024-12-13 13:56:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone_number` varchar(15) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `phone_number`, `address`, `role`) VALUES
+(2, 'arfini', 'arfino123', '+62123456789', 'rumah', 'Buyer'),
+(3, 'petina', 'petina123', '+62123456789', 'serumah', 'Seller'),
+(4, 'test3', 'testakun3', '+62123456789', 'rumah uga', 'Buyer'),
+(5, 'test4', 'testakun4', '+62987654321', 'murah', 'Seller'),
+(7, 'admin', 'admin', '+62123456789', 'sample address', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `wishlist_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -68,6 +131,29 @@ ALTER TABLE `items`
   ADD KEY `seller_id` (`seller_id`);
 
 --
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `buyer_id` (`buyer_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`wishlist_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -75,7 +161,25 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  MODIFY `wishlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -86,6 +190,20 @@ ALTER TABLE `items`
 --
 ALTER TABLE `items`
   ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`);
+
+--
+-- Constraints for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `wishlist_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
